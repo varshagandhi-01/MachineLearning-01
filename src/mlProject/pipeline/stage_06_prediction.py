b73f3aa50eb04c85a67f02ec750afe6006830d85
varshagandhi-01 <varshagandhi@gmail.com>
2025-11-21 16:56:32 +0530

    fixed issue with model path

--- src/mlProject/pipeline/stage_06_prediction.py
+++ src/mlProject/pipeline/stage_06_prediction.py
@@ -5,7 +5,7 @@ import joblib
 
 class PredictionPipeline:
     def __init__ (self):
-        self.model = joblib.load(Path("artifacts\model_trainer\model.joblib"))
+        self.model = joblib.load(Path("artifacts/model_trainer/model.joblib"))
 
     def predict(self, data):
         prediction = self.model.predict(data)
